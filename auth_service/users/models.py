98c4bf97043bc5628ec5b428595b095c6b9d91a4
m.ustiantsev <m.ustiantsev@ostrovok.ru>
2024-03-03 19:09:46 +0300

    some todos for user model

--- auth_service/users/models.py
+++ auth_service/users/models.py
@@ -3,3 +3,7 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     pass
+
+
+# todo добавить role и public_id
+# todo сделать post_save signal на отправку CUD данных по юзеру
