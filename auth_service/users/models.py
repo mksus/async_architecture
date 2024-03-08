539e33cee1af6b8796f7be3258b5fc40a5bd6745
m.ustiantsev <m.ustiantsev@ostrovok.ru>
2024-03-08 12:26:11 +0300

    update user model

--- auth_service/users/models.py
+++ auth_service/users/models.py
@@ -1,9 +1,18 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
+from djchoices import ChoiceItem, DjangoChoices
 
 
 class User(AbstractUser):
-    pass
 
+    class Role(DjangoChoices):
+        admin = ChoiceItem('admin')
+        manager = ChoiceItem('manager')
+        accountant = ChoiceItem('accountant')
+        dev = ChoiceItem('dev')
 
-# todo добавить role и public_id
-# todo сделать post_save signal на отправку CUD данных по юзеру
+    role = models.CharField(choices=Role.choices, max_length=128, default=Role.dev)
+    email = models.EmailField()
+
+    #  AbstractUser.username is public_id because django-oauth-toolkit uses it in /introspect request
+    #  so auth binds users by username in other services
