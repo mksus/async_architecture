169e96af1daff4233f00adf21805faca843f73d1
m.ustiantsev <m.ustiantsev@ostrovok.ru>
2024-03-09 18:29:24 +0300

    schema registry draft

---
+++ event_schema_registry/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='event_schema_registry',
+    version='0.1',
+    url='https://github.com/mksus/async_architecture/events_schema_registry',
+    packages=find_packages(),
+    install_requires=[
+        'jsonschema',
+    ],
+)
