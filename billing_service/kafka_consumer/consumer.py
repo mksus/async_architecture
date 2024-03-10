7b5871867c7ec16ca597e47a2694c5c96aa46944
m.ustiantsev <m.ustiantsev@ostrovok.ru>
2024-03-10 14:38:25 +0300

    maka a single consumer for all topics

--- billing_service/kafka_consumer/consumer.py
+++ billing_service/kafka_consumer/consumer.py
@@ -2,12 +2,17 @@ def run():
     from kafka import KafkaConsumer
     import json
     from auth_client.models import User
+    from billing.models import Task
 
     import event_schema_registry.schemas.auth_service.AccountCreated as account_created_registry
     import jsonschema
 
     ACCOUNTS_STREAM = 'accounts_stream'
     ACCOUNTS = 'accounts'
+
+    TASKS_STREAM = 'tasks_stream'
+    TASKS = 'tasks'
+
 
     consumer = KafkaConsumer(
         bootstrap_servers=['localhost:9092'],
@@ -16,7 +21,7 @@ def run():
         api_version=(2, 0)
     )
 
-    consumer.subscribe(topics=[ACCOUNTS, ACCOUNTS_STREAM])
+    consumer.subscribe(topics=[ACCOUNTS, ACCOUNTS_STREAM, TASKS_STREAM, TASKS])
 
     for message in consumer:
         value = message.value
@@ -57,5 +62,19 @@ def run():
                 user.save(update_fields=["role"])
             except User.DoesNotExist:
                 User.objects.create(**message.value["data"])
+        
+        elif event_name == "TaskCreated":
+            try:
+                # jsonschema.validate(value, account_created_registry.v1)
+                assignee = User.objects.get(username=data['assignee_username'])
+                u = Task.objects.create(
+                    description=data['descriptions'],
+                    status=data['status'],
+                    assignee=assignee,
+                )
+                print(u)
+                u.save()
+            except Exception as e:
+                print(e)
 
 run()
